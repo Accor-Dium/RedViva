149aad1acf7101ac54744dab7e21026514dfe43d
Hiram Abif Cornejo Cornejo <hiramcornejo10@gmail.com>
2026-03-08 01:19:59 -0600

    fix(config): set site URL to https://www.red-viva.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.red-viva.org',
   output: 'server',
   integrations: [react()],
   image: {
